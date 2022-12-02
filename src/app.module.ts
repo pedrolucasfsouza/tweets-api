c8e0e123f8d85a615eaaf20af195bfb0aed575b0
Pedro Lucas <pedrolucasfsouza@gmail.com>
2022-12-02 15:39:04 -0300

    config docker compose prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
@@ -10,7 +11,10 @@ import { TweetsModule } from './tweets/tweets.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      envFilePath: [
+        join(__dirname, '..', `.env.${process.env.NODE_ENV}`),
+        join(__dirname, '..', '.env'),
+      ],
     }),
     TweetsModule,
     MongooseModule.forRoot(process.env.MONGO_DSN),
